ca9c1881b8588acda7485527429181d8c6f33148
Dhanush Achari <119281958+Dhanushachari@users.noreply.github.com>
2023-09-11 10:33:47 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,51 @@
+   let answers = document.querySelectorAll(".accordion");
+   answers.forEach((event) => {
+      event.addEventListener('click', () => {
+         if (event.classList.contains("active")) {
+            event.classList.remove("active");
+         } else {
+            event.classList.add("active");
+         }
+      })
+   })
+
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "50px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+
+
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+
+
+
+
